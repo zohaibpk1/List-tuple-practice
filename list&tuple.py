ba0601b1461d74584d04a21cc9756d65fbbbdcfb
Zohaib Raza <111698841+zohaibpk1@users.noreply.github.com>
2025-07-28 18:56:27 +0500

    Update list&tuple.py
    change this file

--- list&tuple.py
+++ list&tuple.py
@@ -1,102 +1,99 @@
-# marks1 = 23.9
-# marks2 = 44.4
-# marks3 = 21.4
-# marks4 = 89.9
-# marks5 = 33.2
-# marks6 = 10.8
+marks1 = 23.9
+marks2 = 44.4
+marks3 = 21.4
+marks4 = 89.9
+marks5 = 33.2
+marks6 = 10.8
 
-# marks = [23.9, 44.4,21.4, 89.9, 33.2, 10.8]
-# print(marks[3],marks[0])
-# print(marks[1])
-# print(marks)
-# print(type(marks))
-# print(len(marks))
+marks = [23.9, 44.4,21.4, 89.9, 33.2, 10.8]
+print(marks[3],marks[0])
+print(marks[1])
+print(marks)
+print(type(marks))
+ print(len(marks))
 
-# student = ["ZOHIB", 22, "garh more", 95.30,]
-# print(student)
-
-
-# marks = [23, 45, 55, 66, 89]
-# print(marks[1:])
+student = ["ZOHIB", 22, "garh more", 95.30,]
+print(student)
 
 
-# list = [1,2,3,4]
-# list.append(5)
-# print(list)
+marks = [23, 45, 55, 66, 89]
+print(marks[1:])
 
 
-# list = [7,9,6,2,1,5,4,8,3]
-# print(list.sort(reverse=True))
-# print(list)
+list = [1,2,3,4]
+list.append(5)
+ print(list)
 
 
-# list = ['a','e','b','d','f','c']
-# list.reverse()
-# list.sort()
-# print(list)
+list = [7,9,6,2,1,5,4,8,3]
+print(list.sort(reverse=True))
+print(list)
 
 
-# list = [2, 3 ,4, 5]
-# list.insert(0, 1)
-# print(list)
-
-# list = [1,1,2,3,4,5]
-# list.remove(1)
-# print(list)
+list = ['a','e','b','d','f','c']
+list.reverse()
+list.sort()
+print(list)
 
 
-# list = [34,5,6,7,8,65,43,46]
-# print(list)
+list = [2, 3 ,4, 5]
+list.insert(0, 1)
+print(list)
 
-# list = [1,2,3,4,4,5]
-# list.pop(4)
-# print(list)
+list = [1,1,2,3,4,5]
+list.remove(1)
+print(list)
 
-                                # tuples
-# tup = (5,6,7,8,9,10)
-# print(type(tup))
-# print(tup)
+list = [34,5,6,7,8,65,43,46]
+print(list)
 
-# tup = (3,2,4,5,6,7)
-# print(tup[4])
-# print(tup[2])
+list = [1,2,3,4,4,5]
+list.pop(4)
+print(list)
+ #    tuples
+tup = (5,6,7,8,9,10)
+print(type(tup))
+print(tup)
 
-# tup = ()
-# print(type(tup))
-# print(tup)
+tup = (3,2,4,5,6,7)
+print(tup[4])
+print(tup[2])
+
+tup = ()
+print(type(tup))
+print(tup)
 
 
 # practice Q  : 1
-# movies = []
-# mov1 = input("enter 1st movie: ")
-# mov2 = input("enter 2nd movie: ")
-# mov3 = input("enter 3rd movie: ")
+movies = []
+mov1 = input("enter 1st movie: ")
+mov2 = input("enter 2nd movie: ")
+mov3 = input("enter 3rd movie: ")
 
-# movies.append(mov1)
-# movies.append(mov2)
-# movies.append(mov3)
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
 
-# print(movies)
+# practice Q  : 2
 
-# practice Q  : 1
+list2 = [1,2,1]
+list1 = ["m", "a","a","m"]
 
-# list2 = [1,2,1]
-# list1 = ["m", "a","a","m"]
+copy_list2 = list2.copy()
+copy_list2.reverse()
 
-# copy_list2 = list2.copy()
-# copy_list2.reverse()
-
-# if(copy_list2 == list2):
-#     print("PalinDrome")
-# else:
-#     print("NOT palinDrome")
-
-
-# grade = ("C","D","A","A","B","B","A")
-# print(grade.count("A"))
+if(copy_list2 == list2):
+     print("PalinDrome")
+ else:
+     print("NOT palinDrome")
 
 
-# grade = ["C,D,A,A,B,B,A"]
-# grade.sort()
-# print(grade)
+ grade = ("C","D","A","A","B","B","A")
+ print(grade.count("A"))
+
+
+grade = ["C,D,A,A,B,B,A"]
+grade.sort()
+print(grade)
 
